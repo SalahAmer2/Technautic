ff5901a35d8e3a1b6f61112d5414d0de6dbfc69b
SalahAmer2 <salahayman2@gmail.com>
2021-07-31 01:31:01 +0200

    fixed ref bug in DetailPage

--- src/components/Detail/DetailPage.tsx
+++ src/components/Detail/DetailPage.tsx
@@ -1,5 +1,6 @@
 import BurgerIcon from "../BurgerIcon.component";
 import NavBar from "../NavBar.component";
+import { Link } from "react-router-dom";
 
 function Detail() {
   return (
@@ -72,24 +73,24 @@ function Detail() {
                 <div className="tabs-outer">
                   <ul className="nav nav-tabs">
                     <li className="active">
-                      <a id="1" href="#">
+                      <Link id="1" to="#">
                         Info
-                      </a>
+                      </Link>
                     </li>
                     <li>
-                      <a id="4" href="#">
+                      <Link id="4" to="#">
                         Owners
-                      </a>
+                      </Link>
                     </li>
                     <li>
-                      <a id="3" href="#">
+                      <Link id="3" to="#">
                         History
-                      </a>
+                      </Link>
                     </li>
                     <li>
-                      <a id="2" href="#">
+                      <Link id="2" to="#">
                         Bids
-                      </a>
+                      </Link>
                     </li>
                   </ul>
                 </div>
@@ -103,8 +104,8 @@ function Detail() {
                             className="panel-title-img "
                             data-toggle="collapse"
                             data-parent="#accordion"
-                            ref="#collapseOne"
                           >
+                            <Link to="#collapseOne">
                             <img src="assets/images/panel-img-con.png" alt="" />
                             <div className="panel-title-detail">
                               <p>Owners</p>
@@ -113,6 +114,7 @@ function Detail() {
                                 <img src="assets/images/copy.png" alt="" />
                               </span>
                             </div>
+                            </Link>
                           </div>
                         </h4>
                       </div>
@@ -134,19 +136,20 @@ function Detail() {
                             className="panel-title-img "
                             data-toggle="collapse"
                             data-parent="#accordion"
-                            ref="#collapseTwo"
                           >
-                            <img
-                              src="assets/images/top-sellers-img-thir.png"
-                              alt=""
-                            />
-                            <div className="panel-title-detail">
-                              <p>Creater</p>
-                              <span>
-                                Adif Raza{" "}
-                                <img src="assets/images/copy.png" alt="" />
-                              </span>
-                            </div>
+                            <Link to="#collapseTwo">
+                              <img
+                                src="assets/images/top-sellers-img-thir.png"
+                                alt=""
+                              />
+                              <div className="panel-title-detail">
+                                <p>Creater</p>
+                                <span>
+                                  Adif Raza{" "}
+                                  <img src="assets/images/copy.png" alt="" />
+                                </span>
+                              </div>
+                            </Link>
                           </div>
                         </h4>
                       </div>
@@ -165,16 +168,17 @@ function Detail() {
                             className="panel-title-img "
                             data-toggle="collapse"
                             data-parent="#accordion"
-                            ref="#collapseThree"
                           >
-                            <img src="assets/images/panel-img-con.png" alt="" />
-                            <div className="panel-title-detail">
-                              <p>Owners</p>
-                              <span>
-                                0xb620..48hsn{" "}
-                                <img src="assets/images/copy.png" alt="" />
-                              </span>
-                            </div>
+                            <Link to="#collapseThree">
+                              <img src="assets/images/panel-img-con.png" alt="" />
+                              <div className="panel-title-detail">
+                                <p>Owners</p>
+                                <span>
+                                  0xb620..48hsn{" "}
+                                  <img src="assets/images/copy.png" alt="" />
+                                </span>
+                              </div>
+                            </Link>
                           </div>
                         </h4>
                       </div>
@@ -191,140 +195,146 @@ function Detail() {
                     </div>
                   </div>
                 </div>
+
                 <div className="tabContent" id="tabContent2">
                   <div className="tab-2-offers-detail">
                     <p>
-                      <a href="">@waste</a> made an offer of 1.2Ξ
+                      <Link to="">@waste</Link> made an offer of 1.2Ξ
                     </p>
                     <p>
-                      3 HOURS AGO <a href="">[view tx]</a>{" "}
+                      3 HOURS AGO <Link to="">[view tx]</Link>{" "}
                     </p>
                   </div>
                   <div className="tab-2-offers-detail">
                     <p>
-                      <a href="">@waste</a> made an offer of 1.2Ξ
+                      <Link to="">@waste</Link> made an offer of 1.2Ξ
                     </p>
                     <p>
-                      3 HOURS AGO <a href="">[view tx]</a>{" "}
+                      3 HOURS AGO <Link to="">[view tx]</Link>{" "}
                     </p>
                   </div>
                   <div className="tab-2-offers-detail">
                     <p>
-                      <a href="">@waste</a> made an offer of 1.2Ξ
+                      <Link to="">@waste</Link> made an offer of 1.2Ξ
                     </p>
                     <p>
-                      3 HOURS AGO <a href="">[view tx]</a>{" "}
+                      3 HOURS AGO <Link to="">[view tx]</Link>{" "}
                     </p>
                   </div>
                 </div>
 
-                <div className="tabContent" id="tabContent3">
-                  <div
-                    className="panel-title-img "
-                    data-toggle="collapse"
-                    data-parent="#accordion"
-                    ref="#collapseThree"
-                  >
-                    <img src="assets/images/panel-img-con.png" alt="" />
-                    <div className="panel-title-detail">
-                      <p>
-                        Put on sale for <strong>0.13 BNB</strong> 3 hours ago by
-                        L'Avion
-                      </p>
+                  <div className="tabContent" id="tabContent3">
+                    <div
+                      className="panel-title-img "
+                      data-toggle="collapse"
+                      data-parent="#accordion"
+                    >
+                      <Link to="#collapseThree">
+                        <img src="assets/images/panel-img-con.png" alt="" />
+                        <div className="panel-title-detail">
+                          <p>
+                            Put on sale for <strong>0.13 BNB</strong> 3 hours ago by
+                            L'Avion
+                          </p>
+                        </div>
+                      </Link>
+                    </div>
+                    <div
+                      className="panel-title-img "
+                      data-toggle="collapse"
+                      data-parent="#accordion"
+                    >
+                      <Link to="#collapseThree">
+                        <img src="assets/images/panel-img-con.png" alt="" />
+                        <div className="panel-title-detail">
+                          <p>
+                            Put on sale for <strong>0.13 BNB</strong> 3 hours ago by
+                            L'Avion
+                          </p>
+                        </div>
+                      </Link>
                     </div>
                   </div>
-                  <div
-                    className="panel-title-img "
-                    data-toggle="collapse"
-                    data-parent="#accordion"
-                    ref="#collapseThree"
-                  >
-                    <img src="assets/images/panel-img-con.png" alt="" />
-                    <div className="panel-title-detail">
-                      <p>
-                        Put on sale for <strong>0.13 BNB</strong> 3 hours ago by
-                        L'Avion
-                      </p>
-                    </div>
-                  </div>
-                </div>
 
-                <div className="tabContent" id="tabContent4">
-                  <div
-                    className="panel-title-img "
-                    data-toggle="collapse"
-                    data-parent="#accordion"
-                    ref="#collapseFour"
-                  >
-                    <img src="assets/images/panel-img-con.png" alt="" />
+                  <div className="tabContent" id="tabContent4">
+                    <div
+                      className="panel-title-img "
+                      data-toggle="collapse"
+                      data-parent="#accordion"
+                    >
+                      <Link to="#collapseFour">
+                      <img src="assets/images/panel-img-con.png" alt="" />
+                      <div className="row">
+                        <div className="col-6 col-md-6">
+                          <div className="panel-title-detail">
+                            <p>Owners</p>
+                            <span>
+                              0xb620..48hsn{" "}
+                              <Link to="#">
+                                <img src="assets/images/copy.png" alt="" />
+                              </Link>{" "}
+                            </span>
+                          </div>
+                        </div>
+                        <div className="col-6 col-md-6">
+                          <div className="panel-title-detail">
+                            <p>Hold</p>
+                            <span>49945</span>
+                          </div>
+                        </div>
+                      </div>
+                      </Link>
+                    </div>
+                    <div
+                      className="panel-title-img "
+                      data-toggle="collapse"
+                      data-parent="#accordion"
+                    >
+                      <Link to="#collapseFour">
+                        <img src="assets/images/panel-img-con.png" alt="" />
+                        <div className="row">
+                          <div className="col-6 col-md-6">
+                            <div className="panel-title-detail">
+                              <p>Owners</p>
+                              <span>
+                                0xb620..48hsn{" "}
+                                <Link to="#">
+                                  <img src="assets/images/copy.png" alt="" />
+                                </Link>
+                              </span>
+                            </div>
+                          </div>
+                          <div className="col-6 col-md-6">
+                            <div className="panel-title-detail">
+                              <p>Hold</p>
+                              <span>49945</span>
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </div>
+                  </div>
+                  <div className="detail-page-actions">
                     <div className="row">
-                      <div className="col-6 col-md-6">
-                        <div className="panel-title-detail">
-                          <p>Owners</p>
-                          <span>
-                            0xb620..48hsn{" "}
-                            <a href="#">
-                              <img src="assets/images/copy.png" alt="" />
-                            </a>{" "}
-                          </span>
+                      <div className="col-md-4 nopadding">
+                        <div className="buy-now-btn-outer">
+                          <Link to="#">Buy Now</Link>
                         </div>
                       </div>
-                      <div className="col-6 col-md-6">
-                        <div className="panel-title-detail">
-                          <p>Hold</p>
-                          <span>49945</span>
+                      <div className="col-md-4 nopadding">
+                        <div className="buy-now-btn-outer">
+                          <Link to="#">place a bid</Link>
                         </div>
                       </div>
+                      <div className="col-md-4 nopadding"></div>
                     </div>
                   </div>
-                  <div
-                    className="panel-title-img "
-                    data-toggle="collapse"
-                    data-parent="#accordion"
-                    ref="#collapseFour"
-                  >
-                    <img src="assets/images/panel-img-con.png" alt="" />
-                    <div className="row">
-                      <div className="col-6 col-md-6">
-                        <div className="panel-title-detail">
-                          <p>Owners</p>
-                          <span>
-                            0xb620..48hsn{" "}
-                            <a href="#">
-                              <img src="assets/images/copy.png" alt="" />
-                            </a>
-                          </span>
-                        </div>
-                      </div>
-                      <div className="col-6 col-md-6">
-                        <div className="panel-title-detail">
-                          <p>Hold</p>
-                          <span>49945</span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="detail-page-actions">
-                  <div className="row">
-                    <div className="col-md-4 nopadding">
-                      <div className="buy-now-btn-outer">
-                        <a href="#">Buy Now</a>
-                      </div>
-                    </div>
-                    <div className="col-md-4 nopadding">
-                      <div className="buy-now-btn-outer">
-                        <a href="#">place a bid</a>
-                      </div>
-                    </div>
-                    <div className="col-md-4 nopadding"></div>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+
 
       <div id="c-mask-right" className="c-mask"></div>
     </div>
