3edd23365688a157eeea746f4b0658ebf6ed4bd6
SalahAmer2 <salahayman2@gmail.com>
2021-08-01 02:02:52 +0200

    replaced social media icons with SocialMediaIconsReact in ProfileCard component

--- src/components/ProfileCard.component.tsx
+++ src/components/ProfileCard.component.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './ProfileCard.styles.css'
+import {SocialMediaIconsReact} from 'social-media-icons-react';
 
 const ProfileCard = () => {
     return (
@@ -30,17 +31,17 @@ const ProfileCard = () => {
                     <ul className="profile-icons">
                       <li>
                         <a href="#">
-                          <i className="fab fa-facebook-square"></i>
+                          <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" icon="facebook" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(26,166,233,1)" url="https://some-website.com/my-social-media-url" size="20" />
+                        </a>
+                      </li>
+                      <li>
+                        <a className='middle-social-media-icon' href="#">
+                          <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" icon="instagram" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(26,166,233,1)" url="https://some-website.com/my-social-media-url" size="20" />
                         </a>
                       </li>
                       <li>
                         <a href="#">
-                          <i className="fab fa-instagram"></i>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#">
-                          <i className="fab fa-twitter-square"></i>
+                          <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" icon="twitter" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(26,166,233,1)" url="https://some-website.com/my-social-media-url" size="20" />
                         </a>
                       </li>
                     </ul>
